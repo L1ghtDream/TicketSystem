0173ddd379bf741a7a2a8ed427f982f256b366ee
LightDream <radu.voinea.mihai@gmail.com>
2022-02-27 09:00:03 +0200

    check ban command

--- src/main/java/dev/lightdream/ticketsystem/commands/CheckBanCommand.java
+++ src/main/java/dev/lightdream/ticketsystem/commands/CheckBanCommand.java
@@ -2,6 +2,7 @@ package dev.lightdream.ticketsystem.commands;
 
 import dev.lightdream.jdaextension.commands.DiscordCommand;
 import dev.lightdream.ticketsystem.Main;
+import dev.lightdream.ticketsystem.dto.BanRecord;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageChannel;
@@ -16,8 +17,36 @@ public class CheckBanCommand extends DiscordCommand {
     }
 
     @Override
-    public void execute(Member member, TextChannel textChannel, List<String> list) {
-        textChannel.sendMessage("WIP").queue();
+    public void execute(Member member, TextChannel textChannel, List<String> args) {
+        if (args.size() != 1) {
+            sendUsage(textChannel);
+            return;
+        }
+
+        long id;
+        try {
+            id = Long.parseLong(args.get(0));
+        } catch (Exception e) {
+            textChannel.sendMessageEmbeds(Main.instance.jdaConfig.invalidID.build().build()).queue();
+            return;
+        }
+
+        BanRecord ban = Main.instance.databaseManager.getBan(id);
+        if (ban == null) {
+            textChannel.sendMessageEmbeds(Main.instance.jdaConfig.notBanned.build().build()).queue();
+            return;
+        }
+
+        Main.instance.bot.retrieveUserById(id).queue(user ->
+                Main.instance.bot.retrieveUserById(ban.bannedBy).queue(bannedBy ->
+                        textChannel.sendMessageEmbeds(Main.instance.jdaConfig.unbanDetails
+                                .parse("name", user.getName())
+                                .parse("id", user.getId())
+                                .parse("banned_by_name", bannedBy.getName())
+                                .parse("banned_by_id", String.valueOf(bannedBy))
+                                .parse("reason", ban.reason)
+                                .build().build()
+                        ).queue()));
     }
 
     @Override
